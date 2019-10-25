e34e81a55f8444988c515772094d5a683afda129
Qinghang Hong <qhong@umail.ucsb.edu>
2019-10-24 23:30:16 -0700

    implement factorial

--- factorial.asm
+++ factorial.asm
@@ -12,15 +12,39 @@
 #   cout << "Factorial is:\n" << fn << "\n";
 #
 .data
-
-	# TODO: Write your initializations here
-
+	enter: .asciiz "Enter the number:\n"
+	factorial: .asciiz "Factorial is:\n"
+	newLine: .asciiz "\n"
 #Text Area (i.e. instructions)
 .text
 main:
+	li $v0, 4
+	la $a0, enter
+	syscall
+	li $v0, 5
+	syscall
+	move $t0, $v0 #n
 
-	# TODO: Write your code here
-
+	li $t1, 1 #fn
+	li $t2, 2 #x
+	loop:
+	blt $t0, $t2, print
+	mult $t1, $t2
+	mflo $t1
+	addi $t2, $t2, 1
+	j loop
+	
+	print:
+	li $v0, 4
+	la $a0, factorial
+	syscall
+	li $v0, 1
+	move $a0, $t1
+	syscall
+	li $v0, 4
+	la $a0, newLine
+	syscall
+	
 exit:
 	# Exit
 	li $v0, 10
